5fe74e1ad8b91525c04f9bc949699f67c355a41e
shruti910 <shruti.lovelyyy@gmail.com>
2022-03-15 20:33:17 +0530

    first commit

---
+++ websocket-chat-app/src/test/java/com/simplechat/org/LombokTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplechat.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LombokTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
